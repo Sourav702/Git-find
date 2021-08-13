3f1fadb76c6073dceff21a05ddd17de7bbd3e9ce
Sourav Naga <sourav.naga@kreeti.com>
2021-08-13 23:32:25 +0530

    create alert message

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 $(document).ready(function () {
+  $("#alert-item").hide();
   $(".btn").click(function () {
     let user = $("#find").val();
     // alert(user);
-    $.get(" https://api.github.com/users/" + user, function (data, status) {
-      // alert("Data: " + data.login + "\nStatus: " + status);
-      // $("#search").val(data.avatar_url);
-      $("#image").attr("src", data.avatar_url);
-      $(".card-title").html(data.name);
-      $(".card-text").html(data.bio);
-      $(".card-repo").html("Total Repo : " + data.public_repos);
-      let profile = "https://github.com/" + user;
+    if (user == "") {
+      $("#inside-alert").text("User Name Not Blank :) ");
+      $("#alert-item").show();
+      $("#image").attr("src", "");
+      $(".card-title").html("");
+      $(".card-text").html("");
+      $(".card-repo").html("");
+      let profile = "https://github.com/";
       $("#go").attr("href", profile);
-      window.data = data;
-    });
+      console.log("textbox not empty");
+    } else {
+      $("#alert-item").hide();
+      $.get(" https://api.github.com/users/" + user, function (data, status) {
+        // alert("Data: " + data.login + "\nStatus: " + status);
+        // $("#search").val(data.avatar_url);
+        $("#image").attr("src", data.avatar_url);
+        $(".card-title").html(data.name);
+        $(".card-text").html(data.bio);
+        $(".card-repo").html("Total Repo : " + data.public_repos);
+        let profile = "https://github.com/" + user;
+        $("#go").attr("href", profile);
+        window.data = data;
+      });
+    }
   });
   $("#add").click(function () {
     // alert($(".card-title").text());
